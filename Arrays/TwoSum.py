ba3a997eb8abff7d7981831f43fbcd1cb713ba8c
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-10-01 01:02:15 -0500

    Add loops to get all possible two pairs

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -29,3 +29,9 @@ Minimum length of nums is 2
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        
+        for i in range(len(nums)):
+
+            for j in  range(i + 1, len(nums)):
+
+                
