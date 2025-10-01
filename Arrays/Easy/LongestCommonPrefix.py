f70e8be85f256130f9b4fd7163ff0841bb2bbc73
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-10-01 17:51:52 -0500

    Add check to return common prefix

--- Arrays/Easy/LongestCommonPrefix.py
+++ Arrays/Easy/LongestCommonPrefix.py
@@ -40,6 +40,7 @@ class Solution:
         for i in range(len(strs[0])):
             
             for j in range(1, len(strs)):
-                
+                if i > len(strs[j]) - 1 or strs[j][i] != strs[0][i]:
+                    return strs[0][:i]
         return strs[0]
 
