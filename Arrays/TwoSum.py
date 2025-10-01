d882dd84713b833080fe44ca35053e0e1bbd8a44
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-10-01 00:59:30 -0500

    Add Brute Force

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -9,5 +9,23 @@ and you may not use the same element twice
 Come up with an algorithm that is less than O(n**2) time complexity
 """
 
+"""
+Thought Process:
+Aim: Get indices of two numbers that sum up to the target
+
+Brute Force:
+- Find all possible num pairs
+- Get each of their sum
+- if sum  == target:
+      return indices of each num in pair
+  
+
+Minimum length of nums is 2
+
+
+"""
+
+
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
