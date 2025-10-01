430b524b777e9ea5a1a5c7d2f9d6d0762be399a6
Oluwaseyi Popoola <oluwaseyipopoola49@gmail.com>
2025-10-01 00:49:23 -0500

    Add leetcode problem and initial code

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -1,1 +1,13 @@
+"""
+Problem:
+Given an array of integer nums and an integer target, 
+return indices of two numbers such that they add up to target. 
 
+You may assume that each input would have exactly one solution, 
+and you may not use the same element twice
+
+Come up with an algorithm that is less than O(n**2) time complexity
+"""
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
